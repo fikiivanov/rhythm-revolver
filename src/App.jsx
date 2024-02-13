30b0bdaa43133fc6bb1628118e1d8154e382ca5d
Piko327 <fikii.business@gmail.com>
2024-02-13 14:13:33 +0200

    Add instruments-list HTML and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { FaPlay } from "react-icons/fa";
 import './App.scss'
+import { VectorInstruments } from "./utils/data";
 function App() {
 
 
@@ -9,18 +10,17 @@ function App() {
             <div className="shell">
                 <nav className="header">
                     <ul >
-                        <li> <a to="#instroments">instroments</a></li>
+                        <li> <a href="#instruments">instroments</a></li>
 
-                        <li> <a to="#effects">effects</a></li>
+                        <li> <a href="#effects">effects</a></li>
 
-                        <li> <a to="#play">play</a></li>
+                        <li> <a href="#play">play</a></li>
                     </ul>
                 </nav>
             </div>
 
             <section className="hero">
-
-                    <img src='./assets/backgroundHero.jpg' alt="" />
+                <img src='./assets/backgroundHero.jpg' alt="" />
 
                 <div className="shell">
                     <div className="hero__inner">
@@ -35,40 +35,24 @@ function App() {
                 </div >
             </section>
 
-            {/* <section id='instruments' className="instrument-list">
+            <section id='instruments' className="instrument-list">
                 <div className="shell">
-                    <h2 className='instrument-list__title'>Embark on a Melodic Journey: Explore What We've Crafted for You.</h2>
+                    <div className="instrument-list__inner">
+                        <h2 >Embark on a Melodic Journey: Explore What We've Crafted for You.</h2>
 
-                    <h4>Enter a world of melody. Explore our curated instruments, chosen to ignite your creativity. Find your perfect sound and unlock endless possibilities.</h4>
+                        <h4>Enter a world of melody. Explore our curated instruments, chosen to ignite your creativity. Find your perfect sound and unlock endless possibilities.</h4>
 
-                    <ul>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                        <li>
-                            <div></div>
-                        </li>
-                    </ul>
+                        <ul>
+                            {VectorInstruments.map(({ id, title, path }) =>
+                                <div className="instrument" id={id}>
+                                    <h3>{title}</h3>
+                                    <img src={path} alt="" />
+                                </div>
+                            )}
+                        </ul>
+                    </div>
                 </div>
-            </section> */}
+            </section>
         </div>
 
     );
---
+++ src/utils/data.js
@@ -0,0 +1,49 @@
+
+export const VectorInstruments = [
+    {
+        id: 1,
+        title: "violin",
+        path: './assets/effectsVectors/Asset 1.svg',
+    },
+    {
+        id: 2,
+        title: "guitar",
+        path: './assets/effectsVectors/Asset 2.svg',
+
+    },
+    {
+        id: 3,
+        title: "rhodes",
+        path: './assets/effectsVectors/Asset 3.png',
+
+    },
+    {
+        id: 4,
+        title: "piano",
+        path: './assets/effectsVectors/Asset 4.svg',
+
+    },
+    {
+        id: 5,
+        title: "bass guitar",
+        path: './assets/effectsVectors/Asset 5.svg',
+
+    },
+    {
+        id: 6,
+        title: "flute",
+        path: './assets/effectsVectors/Asset 6.svg',
+
+    },
+    {
+        id: 7,
+        title: "cello",
+        path: './assets/effectsVectors/Asset 7.svg',
+
+    },
+    {
+        id: 8,
+        title: "harp",
+        path: './assets/effectsVectors/Asset 8.svg',
+    },
+]
