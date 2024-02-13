40c546baccf6e515deb27fe7ff4d7567c7164311
Piko327 <fikii.business@gmail.com>
2024-02-13 12:41:18 +0200

    Add hero section HTML and SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
 
-import './App.css';
-
+import { FaPlay } from "react-icons/fa";
+import './App.scss'
 function App() {
 
 
     return (
-        <div className='container'>
-            Hello world
+        <div className="app">
+            <div className="shell">
+                <nav className="header">
+                    <ul >
+                        <li> <a to="#instroments">instroments</a></li>
+
+                        <li> <a to="#effects">effects</a></li>
+
+                        <li> <a to="#play">play</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <section className="hero">
+
+                    <img src='./assets/backgroundHero.jpg' alt="" />
+
+                <div className="shell">
+                    <div className="hero__inner">
+                        <h1 >
+                            Explore a Symphony of Instruments: Let's Play with Music!
+                        </h1>
+
+                        <button>
+                            play <FaPlay />
+                        </button>
+                    </div>
+                </div >
+            </section>
+
+            {/* <section id='instruments' className="instrument-list">
+                <div className="shell">
+                    <h2 className='instrument-list__title'>Embark on a Melodic Journey: Explore What We've Crafted for You.</h2>
+
+                    <h4>Enter a world of melody. Explore our curated instruments, chosen to ignite your creativity. Find your perfect sound and unlock endless possibilities.</h4>
+
+                    <ul>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                        <li>
+                            <div></div>
+                        </li>
+                    </ul>
+                </div>
+            </section> */}
         </div>
+
     );
 }
 
